0e7dd2ebff4fcc068a8230a96485c6d52fc5ffa2
Loc Nguyen <13.tan.loc.nguyen@gmail.com>
2025-11-26 08:24:21 -0600

    More request
    --Top 5 Selling Artists
    --Identify the lowest-selling genre (by dollar revenue) for customers residing in cities in Germany
    --Ranking Customers by Purchase Frequency

--- Script.sql
+++ Script.sql
@@ -49,4 +49,70 @@ WHERE
 	GenreRank = 1
 ORDER BY
 	Country ASC;
-	
+
+--Top 5 Selling Artists
+SELECT 
+	a.Name,
+	SUM(il.Quantity) AS TrackSold
+FROM
+	Artist a
+JOIN Album a2 ON a.ArtistId = a2.ArtistId
+JOIN Track t ON a2.AlbumId = t.AlbumId 
+JOIN InvoiceLine il ON il.TrackId = t.TrackId
+GROUP BY 
+	a.Name
+ORDER BY
+	TrackSold DESC
+LIMIT 5;
+
+--Identify the lowest-selling genre (by dollar revenue) for customers residing in cities in Germany
+SELECT
+	c.City,
+	g.Name as GenreName,
+	SUM(il.UnitPrice *il.Quantity ) AS TotalRevenue
+FROM
+	Customer c
+JOIN Invoice i ON i.CustomerId = c.CustomerId
+JOIN InvoiceLine il ON il.InvoiceId = i.InvoiceId
+JOIN Track t ON t.TrackId = il.TrackId 
+JOIN Genre g ON t.GenreId = g.GenreId 
+WHERE
+	c.Country = "Germany"
+GROUP BY
+	c.City,
+	g.Name
+ORDER BY
+	TotalRevenue ASC;
+
+--Ranking Customers by Purchase Frequency
+WITH CustomerInvoiceCount AS (
+	SELECT
+		c.CustomerId ,
+		c.Country,
+		COUNT(i.InvoiceId) AS InvoiceCount
+	FROM Customer c
+	JOIN Invoice i ON c.CustomerId = i.CustomerId 
+	GROUP BY c.CustomerId,
+	c.Country 
+),
+RankedCustomer AS (
+	SELECT
+		CustomerId,
+		Country,
+		InvoiceCount,
+		DENSE_RANK() OVER (PARTITION BY Country ORDER BY InvoiceCount DESC) as Rank
+	FROM 
+		CustomerInvoiceCount 
+)
+SELECT
+	c.FirstName || " " || c.LastName AS CustomerName,
+	rc.Country,
+	rc.InvoiceCount
+FROM 
+	RankedCustomer rc
+JOIN Customer c ON c.CustomerId = rc.CustomerId
+WHERE
+	rc.Rank = 1
+ORDER BY
+	rc.Country ASC;
+
